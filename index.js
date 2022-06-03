410975e1c000f7045e9a8f8b5bc5175fbe0e97a9
Peter Kimani <peterjajakim@gmail.com>
2022-06-03 16:22:40 +0300

    commit tree bba1e749ad6a81c6afd89342d3f06bb81036400a

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Peter is the Champion!";
+newHeader.className = "victory";
